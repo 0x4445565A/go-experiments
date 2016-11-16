54f1db98c5b087427df5529792663959afdc1955
Brandon Martinez <root@ibreak.systems>
2016-11-17 05:35:50 +0700

    Experimenting at 5am on a plane with implimenting stacks y queues, for reasons.

---
+++ stacksAndQueues.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+  "fmt"
+)
+
+// FILO
+type Stack struct {
+  value int
+  next  *Stack
+}
+
+func (s *Stack) add(i int) {
+  if s.next == nil {
+    s.next = &Stack{value: i}
+  } else {
+    s.next.add(i)
+  }
+}
+
+func (s *Stack) pop() bool {
+  if s.next == nil {
+    return true
+  } else {
+    pop := s.next.pop()
+    if pop {
+      s.next = nil
+    }
+  }
+  return false
+}
+
+func (s *Stack) print() {
+  fmt.Println(s.value)
+  if s.next != nil {
+    s.next.print()
+  }
+}
+
+// FIFO
+type Queue struct {
+  value int
+  next  *Queue
+}
+
+func (q *Queue) add(i int) {
+  if q.next == nil {
+    q.next = &Queue{value: i}
+  } else {
+    q.next.add(i)
+  }
+}
+
+func (q Queue) pop() Queue {
+  q = *q.next
+  return q
+}
+
+func (q *Queue) print() {
+  fmt.Println(q.value)
+  if q.next != nil {
+    q.next.print()
+  }
+}
+
+func main() {
+  q := Queue{value: 1}
+  q.add(2)
+  q.add(3)
+  q.add(4)
+  q.print()
+  q = q.pop()
+  q = q.pop()
+  q.print()
+  fmt.Println("Stacks on stacks")
+  s := Stack{value: 1}
+  s.add(2)
+  s.add(3)
+  s.add(4)
+  s.add(5)
+  s.print()
+  s.pop()
+  s.pop()
+  s.print()
+}
